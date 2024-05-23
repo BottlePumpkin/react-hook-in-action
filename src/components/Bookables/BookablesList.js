a9c69327ee6503ffbed0a5b80fed04ecb266936b
BottlePumpkin <61003485+BottlePumpkin@users.noreply.github.com>
2024-05-23 17:57:34 +0900

    2024.05.23

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,16 +1,38 @@
+import { useState } from "react";
 import data from "../../static.json"
+import { FaArrowRight } from "react-icons/fa";
 
 const {bookables} = data;
 
+function getUniqueValues (array, property) {
+  const propValues = array.map(element => element[property]);
+  const uniqueValues = new Set(propValues);
+  const uniqueValuesArray = [...uniqueValues];
+
+  return uniqueValuesArray;
+}
+
 export default function BookablesList () {
-
-  const group = "Rooms";
-
+  const [group, setGroup] = useState("Kit")
   const bookablesInGroup = bookables.filter(b => b.group === group);
+  const [bookableIndex, setBookableIndex] = useState(1);
+  const groups = getUniqueValues(bookables, "group");
 
-  const bookableIndex = 1;
+  function nextBookable() {
+    setBookableIndex(i => (i + 1) % bookablesInGroup.length);
+  }
 
   return (
+    <div>
+      <select
+        value={group}
+        onChange={
+          (e) => setGroup(e.target.value)}
+      >
+      {groups.map(g => <option value={g} key={g}>{g}</option>)}
+      </select>
+
+
     <ul className="bookables items-list-nav">
       {bookablesInGroup.map((b, i) => (
         <li
@@ -19,11 +41,23 @@ export default function BookablesList () {
         >
           <button
             className="btn"
+            onClick={() => setBookableIndex(i)}
           >
             {b.title}
           </button>
         </li>
       ))}
     </ul>
+    <p>
+      <button
+        className="btn"
+        onClick={nextBookable}
+        autoFocus
+      >
+        <FaArrowRight/>
+        <span>Next</span>
+      </button>
+    </p>
+    </div>
   );
 }
