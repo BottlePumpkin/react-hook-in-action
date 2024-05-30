0037cad6d090f56f2a9c3724811fe16a414e5f80
BottlePumpkin <61003485+BottlePumpkin@users.noreply.github.com>
2024-05-30 17:20:27 +0900

    Update BookablesList.js
    05.30

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState,Fragment } from "react";
 import data from "../../static.json"
 import { FaArrowRight } from "react-icons/fa";
 
-const {bookables} = data;
+const {bookables,sessions,days} = data;
 
 function getUniqueValues (array, property) {
   const propValues = array.map(element => element[property]);
@@ -18,11 +18,16 @@ export default function BookablesList () {
   const [bookableIndex, setBookableIndex] = useState(1);
   const groups = getUniqueValues(bookables, "group");
 
+  const bookable = bookablesInGroup[bookableIndex];
+
+  const [hasDetails, setHasDetails] = useState(false);
+
   function nextBookable() {
     setBookableIndex(i => (i + 1) % bookablesInGroup.length);
   }
 
   return (
+    <Fragment>
     <div>
       <select
         value={group}
@@ -59,5 +64,44 @@ export default function BookablesList () {
       </button>
     </p>
     </div>
+    {bookable && (
+      <div className="bookable=details">
+        <div className="item">
+          <h2>
+            {bookable.title}
+          </h2>
+          <span className="controls">
+            <label>
+              <input type="checkbox"
+              checked={hasDetails}
+              onChange={() => setHasDetails(has => !has)}
+              >
+              </input>
+            </label>
+          </span>
+        </div>
+
+      </div>
+    )}
+            {hasDetails && (
+              <div className="item-details">
+                <h3>Availability</h3>
+                <div className="bookable-availability">
+                  <ul>
+                    {bookable.days
+                      .sort()
+                      .map(d => <li key={d}>{days[d]}</li>)
+                    }
+                  </ul>
+                  <ul>
+                    {bookable.sessions
+                      .map(s => <li key={s}>{sessions[s]}</li>)
+                    }
+                  </ul>
+                </div>
+              </div>
+            )}
+    </Fragment>
+  
   );
 }
